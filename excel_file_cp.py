727967de0e373d8fec5633191c46dac7e3814ff5
znd768 <rasksk37@gmail.com>
2025-05-31 16:51:58 +0900

    main file

---
+++ excel_file_cp.py
@@ -0,0 +1,54 @@
+import csv
+import os
+import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
+ORIGINAL_EXCEL_PATH = os.getenv("ORIGINAL_EXCEL_PATH")
+FOLDER_PATH = os.getenv("FOLDER_PATH")
+
+def read_input():
+    ret = []
+    try:
+        with open("input.csv", "r") as f:
+            csv_reader = csv.reader(f)
+            for row in csv_reader:
+                if row and row[0]:
+                    ret.append(row[0].strip())
+        return ret
+    except FileNotFoundError:
+        print("csv file not found")
+        exit()
+
+def xlsx_copy(source_path, dest_path):
+    try:
+        # copy file
+        shutil.copy(source_path, dest_path)
+    except FileNotFoundError:
+        print("original file not found")
+        exit()
+    except Exception as e:
+        print(e)
+        exit()
+
+def xlsx_rename(old_path, new_name):
+    try:
+        new_path = os.path.join(FOLDER_PATH, new_name)
+        os.rename(old_path, new_path)
+    except FileNotFoundError:
+        print("rename file not found")
+        exit()
+    except Exception as e:
+        print(e)
+        exit()
+
+if __name__ == '__main__':
+    names = read_input()
+
+    if not os.path.exists(FOLDER_PATH):
+        os.makedirs(FOLDER_PATH)
+
+    for num, name in enumerate(names, start=1):
+        tmp_path = os.path.join(FOLDER_PATH, f"tmp_{num}.xlsx")
+        xlsx_copy(ORIGINAL_EXCEL_PATH, tmp_path)
+        xlsx_rename(tmp_path, f"{num}_{name}.xlsx")
