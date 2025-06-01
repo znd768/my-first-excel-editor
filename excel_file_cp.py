e3179d875f0bb438c201bbaaa30027efaba9c053
znd768 <rasksk37@gmail.com>
2025-06-01 19:41:45 +0900

    create main.py

--- excel_file_cp.py
+++ excel_file_cp.py
@@ -1,4 +1,3 @@
-import csv
 import os
 import shutil
 from dotenv import load_dotenv
@@ -6,19 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 ORIGINAL_EXCEL_PATH = os.getenv("ORIGINAL_EXCEL_PATH")
 FOLDER_PATH = os.getenv("FOLDER_PATH")
-
-def read_input():
-    ret = []
-    keys = ["name", "repository_name", "branch_name"]
-    try:
-        with open("input.csv", "r") as f:
-            csv_reader = csv.reader(f)
-            for row in csv_reader:
-                ret.append(dict(zip(keys, [val.strip() for val in row])))
-        return ret
-    except FileNotFoundError:
-        print("csv file not found")
-        exit()
 
 def xlsx_copy(source_path, dest_path):
     try:
@@ -42,16 +28,11 @@ def xlsx_rename(old_path, new_name):
         print(e)
         exit()
 
-def create_xlsx():
-    csv_read = read_input()
-
+def create_xlsx(names):
     if not os.path.exists(FOLDER_PATH):
         os.makedirs(FOLDER_PATH)
 
-    for num, row in enumerate(csv_read, start=1):
+    for num, name in enumerate(names, start=1):
         tmp_path = os.path.join(FOLDER_PATH, f"tmp_{num}.xlsx")
         xlsx_copy(ORIGINAL_EXCEL_PATH, tmp_path)
-        xlsx_rename(tmp_path, f"{num}_{row["name"]}.xlsx")
-
-if __name__ == '__main__':
-    create_xlsx()
+        xlsx_rename(tmp_path, f"{num}_{name}.xlsx")
---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+from excel_file_cp import create_xlsx
+
+def read_input():
+    ret = []
+    keys = ["name", "repository_name", "branch_name"]
+    try:
+        with open("input.csv", "r") as f:
+            csv_reader = csv.reader(f)
+            for row in csv_reader:
+                ret.append(dict(zip(keys, [val.strip() for val in row])))
+        return ret
+    except FileNotFoundError:
+        print("csv file not found")
+        exit()
+
+if __name__ == '__main__':
+    csv_read = read_input()
+    create_xlsx([val["name"] for val in csv_read])
